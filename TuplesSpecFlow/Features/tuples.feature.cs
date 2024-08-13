037d5d2d8840b462b084173d11a21c5260c6e29b
d-fyz <dfyzgames@gmail.com>
2024-08-13 22:50:18 +0300

    Rename project SpecFlowTuples to TuplesSpecFlow

--- TuplesSpecFlow/Features/tuples.feature.cs
+++ TuplesSpecFlow/Features/tuples.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace SpecFlowTuples.Features
+namespace TuplesSpecFlow.Features
 {
     using TechTalk.SpecFlow;
     using System;
